acdfa526fcc72bb12a460e7e50071aa8a135fbf1
gitforodin <112388027+forodinproject@users.noreply.github.com>
2026-06-11 18:37:45 +0530

    Implement a simple grid creation

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+let size = 16;
+for (let i = 0; i < size * size; i++) {
+    const div = document.createElement('div');
+    container.appendChild(div);
+
+}
+
